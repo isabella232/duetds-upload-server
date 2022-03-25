c8a87b8841e2ed5555c8f53cce75bdbc9ff7c51d
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2022-03-25 12:59:07 +0100

    wip

---
+++ config/development.js
@@ -0,0 +1,15 @@
+const defSettings = require("./default")
+const watching = require("./watching")
+
+const config = Object.assign(
+  {},
+  defSettings,
+  {
+    configName: "development",
+    env: "development",
+  },
+  watching
+)
+
+// export config object via module.export
+module.exports = config
